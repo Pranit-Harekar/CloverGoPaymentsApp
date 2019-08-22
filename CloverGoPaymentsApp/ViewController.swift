70064d713b88f7b8a33f0744524d5aeb5d40ec14
Pranit-Harekar <pranitharekar@hotmail.com>
2019-08-22 10:38:08 -0400

    Initial Commit

---
+++ CloverGoPaymentsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloverGoPaymentsApp
+//
+//  Created by Pranit Harekar on 8/22/19.
+//  Copyright © 2019 Pranit Harekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
